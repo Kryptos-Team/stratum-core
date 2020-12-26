c2ad1dde44f77c1be3d2b4e60071db9c4999cae6
Abhimanyu Saharan <desk.abhimanyu@gmail.com>
2020-12-26 20:51:00 +0530

    Added setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,4 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 from src.version import __version__
 
-setup(
-    name="stratum-core",
-    description="Stratum server implementation",
-    author="Abhimanyu Saharan",
-    author_email="desk.abhimanyu@gmail.com",
-    url="https://github.com/Kryptos-Team/stratum-core.git",
-    packages=["src"],
-    zip_safe=False,
-    install_requires=["twisted", "ecdsa", "autobahn"],
-    version=__version__
-)
+setup()
