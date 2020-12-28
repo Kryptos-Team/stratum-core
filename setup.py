0815744bb144ba181d20c08191469441b3023ee4
Abhimanyu Saharan <desk.abhimanyu@gmail.com>
2020-12-28 12:48:01 +0530

    Fixed requirements install for setup.py

--- setup.py
+++ setup.py
@@ -8,7 +8,12 @@ with open(os.path.join(package_root, "src/version.py")) as fp:
     exec(fp.read(), version)
 version = version["__version__"]
 
+with open('./requirements.txt') as r:
+    # strip fixed version info from requirements file
+    requirements = [line.split('=', 1)[0] for line in r]
+
 setup(
+    install_requires=requirements,
     packages=find_packages(),
     version=version
 )
