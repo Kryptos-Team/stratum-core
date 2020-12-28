d4a3ab6ccf95fcec9acf1258963a7a2f7af0b4ff
Abhimanyu Saharan <desk.abhimanyu@gmail.com>
2020-12-28 12:23:52 +0530

    Added core files

---
+++ src/event_handler.py
@@ -0,0 +1,17 @@
+from twisted.internet import defer
+import logzero
+from logzero import logger
+from .services import wrap_result_object
+from .exceptions import MethodNotFound
+import settings
+
+logzero.loglevel(settings.log["level"])
+
+
+class GenericEventHandler(object):
+    def _handle_event(self, msg_method, msg_params, connection_ref):
+        return defer.maybeDeferred(wrap_result_object, self.handle_event(msg_method, msg_params, connection_ref))
+
+    def handle_event(self, msg_method, msg_params, connection_ref):
+        logger.error(f"Other side called method {msg_method} with params {msg_params}")
+        raise MethodNotFound(f"Method {msg_method} not implemented")
--- src/server.py
+++
@@ -1,4 +0,0 @@
-from .log import Logger
-
-log = Logger(name=__name__, tag="stratum.server")
-log.debug("Test")
